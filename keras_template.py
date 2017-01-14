a568e07bdc10d6f953144802d2f85157c04a04b5
Huangweihang <huangweihang14@mails.ucas.ac.cn>
2017-01-14 02:57:11 -0500

    add keras and tensorflow template

---
+++ keras_template.py
@@ -0,0 +1,19 @@
+# generate dummy data
+import numpy as np
+from keras.layers import Dense
+from keras.models import Sequential
+
+# for a single-input model with 2 classes (binary):
+model = Sequential()
+model.add(Dense(1, input_dim=784, activation='sigmoid'))
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+data = np.random.random((1000, 784))
+labels = np.random.randint(2, size=(1000, 1))
+
+# train the model, iterating on the data in batches
+# of 32 samples
+model.fit(data, labels, nb_epoch=10, batch_size=32)
+
